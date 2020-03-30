8b1142ce8d08e43414b1e48ffe3e377cf6a1467e
Kasper10100 <v.grinkov@ergius.ru>
2020-03-30 18:48:42 +0300

    vendig_machine

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Cards from './components/Cards';
+import ControlPanel from './components/ControlPanel';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <div className="page__showcase">
+        <div className="page__showcase-body">
+          <ul className="product-list">
+            <Cards />
+          </ul>
+        </div>
+      </div>
+      <ControlPanel />
     </div>
   );
 }
